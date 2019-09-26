09483e6547e3d78701656d4ac44b481caa74618f
iwata008 <iwata008@morris.umn.edu>
2019-09-25 22:40:57 -0500

    Inject HttpCliient into the constructor of the CartService component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,6 +8,10 @@ import { HttpClient } from '@angular/common/http';
 export class CartService {
   items = [];
 
+  constructor(
+    private http: HttpClient
+  ) {}
+
   addToCart(product) {
     this.items.push(product);
   }
